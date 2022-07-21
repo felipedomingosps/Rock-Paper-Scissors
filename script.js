466606e4b6c3388f6f7183a584b4980b41e66dcc
Felipe Domingos <felipedomingosps@gmail.com>
2022-07-21 18:15:51 -0300

    Add game() function

--- script.js
+++ script.js
@@ -6,8 +6,10 @@ function getComputerChoice() {
 function playRound(playerSelection, computerSelection) {
     // Make playerSelection case insensitive
     playerSelection = playerSelection.toLowerCase(); 
+
     // Establish the game rules
     let selection = {'rock':'scissors', 'scissors':'paper', 'paper':'rock'};
+
     // Define who won the round
     let winnerSelection;
     if (selection[playerSelection] === computerSelection) {
@@ -15,24 +17,53 @@ function playRound(playerSelection, computerSelection) {
     } else {
         winnerSelection = computerSelection;
     }
+
     // Return the message
     if (playerSelection === computerSelection) {
-        return 'Tie!';
+        return 'tie';
     } else if (winnerSelection === playerSelection) {
-        // Capitalize the first letter of the winner selection
-        playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1);
-        // Return the result
-        return `You won! ${playerSelection} beats ${computerSelection}.`;
+        return `win`;
     } else if (winnerSelection === computerSelection){
-        computerSelection = computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1);
-        return `You lose! ${computerSelection} beats ${playerSelection}.`; 
+        return `lose`; 
     }
 }
 
-/* Algorithm
+function game() {
+    // Define score variables
+    let playerScore = 0;
+    let computerScore = 0;
 
-> Take playerSelection and computerSelection values.
-> Establish the rules of the game
-> Compare playerSelection and computerSelection and sees who win
+    //Create for loop to play 5 rounds
+    for (let i = 1; i < 6; i++) {
+        //Create selection variables
+        let playerSelection = prompt("Rock, paper or scissors?");
+        let computerSelection = getComputerChoice()
+        
+        //Play Round
+        let result = playRound(playerSelection, computerSelection);
 
-*/
+        // Capitalize the first letter of the variables values
+        playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1);
+        computerSelection = computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1);
+
+        //See who wins
+        if (result == 'win') {
+            playerScore += 1;
+            alert(`You win! ${playerSelection} beats ${computerSelection}. \n \n SCORE \n You: ${playerScore} \n PC: ${computerScore}`)
+        } else if (result == 'lose') {
+            computerScore += 1;
+            alert(`You lose! ${computerSelection} beats ${playerSelection}. \n \n SCORE \n You: ${playerScore} \n PC: ${computerScore}`)
+        } else {
+            alert(`Tie! \n \n SCORE \n You: ${playerScore} \n PC: ${computerScore}`)
+        }
+    }
+
+    //Show Game Over box
+    if (playerScore === computerScore) {
+        alert(`GAME OVER! \n \n You: ${playerScore} \n PC: ${computerScore} \n None of you won!`)
+    } else if (playerScore > computerScore) {
+        alert(`GAME OVER! \n \n You: ${playerScore} \n PC: ${computerScore} \n You won!`)
+    } else {
+        alert(`GAME OVER! \n \n You: ${playerScore} \n PC: ${computerScore} \n You lost!`)
+    }
+}

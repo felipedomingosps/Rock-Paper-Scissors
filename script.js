a93b9374ec219a39067804f9418df44c1fd40afe
Felipe Domingos <Felipedomingosps@gmail.com>
2022-08-01 18:59:22 -0300

    Organize functions

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
+//Returns a random valor of the array [rock, paper, scissors]
 function getComputerChoice() {
     let choices = ['', 'rock', 'paper', 'scissors'];
     return choices[Math.floor(Math.random()*(3-1+1))+1]
 }
 
+// Play one round between the player and the PC
 function playRound(playerSelection, computerSelection) {
-    // Make playerSelection case insensitive
-    playerSelection = playerSelection.toLowerCase(); 
-
     // Establish the game rules
     let selection = {'rock':'scissors', 'scissors':'paper', 'paper':'rock'};
 
@@ -16,7 +15,10 @@ function playRound(playerSelection, computerSelection) {
         winnerSelection = playerSelection;
     } else {
         winnerSelection = computerSelection;
-    }
+    };
+
+    // Call round animation function
+    roundAnimation(winnerSelection, playerSelection, computerSelection);
 
     // Return the message
     if (playerSelection === computerSelection) {
@@ -25,53 +27,25 @@ function playRound(playerSelection, computerSelection) {
         return `win`;
     } else if (winnerSelection === computerSelection){
         return `lose`; 
-    }
+    };
+}
+
+// Animates the display after a round is finished
+function roundAnimation(winner, player, pc) {
+    //...
+}
+
+// See who won the entire game
+function getWinner(player,pc) {
+    //...
 }
 
 function game() {
-    // Define score variables
-    let playerScore = 0;
-    let computerScore = 0;
-
-    //Create for loop to play 5 rounds
-    for (let i = 1; i < 6; i++) {
-        //Create selection variables
-        let playerSelection = prompt("Rock, paper or scissors?");
-        let computerSelection = getComputerChoice()
-        
-        //Play Round
-        let result = playRound(playerSelection, computerSelection);
-
-        // Capitalize the first letter of the variables values
-        playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1);
-        computerSelection = computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1);
-
-        //See who wins
-        if (result == 'win') {
-            playerScore += 1;
-            alert(`You win! ${playerSelection} beats ${computerSelection}. \n \n SCORE \n You: ${playerScore} \n PC: ${computerScore}`)
-        } else if (result == 'lose') {
-            computerScore += 1;
-            alert(`You lose! ${computerSelection} beats ${playerSelection}. \n \n SCORE \n You: ${playerScore} \n PC: ${computerScore}`)
-        } else {
-            alert(`Tie! \n \n SCORE \n You: ${playerScore} \n PC: ${computerScore}`)
-        }
-    }
-
-    //Show Game Over box
-    if (playerScore === computerScore) {
-        alert(`GAME OVER! \n \n You: ${playerScore} \n PC: ${computerScore} \n None of you won!`)
-    } else if (playerScore > computerScore) {
-        alert(`GAME OVER! \n \n You: ${playerScore} \n PC: ${computerScore} \n You won!`)
-    } else {
-        alert(`GAME OVER! \n \n You: ${playerScore} \n PC: ${computerScore} \n You lost!`)
-    }
+    //Return the id of the clicked choice
+    const choices = Array.from(document.getElementsByClassName('choice__img'));
+    choices.forEach(item => item.addEventListener('click', function(e) {
+        return e.target.id
+    }))
+    //...
 }
 
-// -------------DOM manipulation
-
-//Event that listen to the choice clicked by the player
-const choices = Array.from(document.getElementsByClassName('choice__img'));
-choices.forEach(item => item.addEventListener('click', function(e) {
-    return e.target.id
-}))

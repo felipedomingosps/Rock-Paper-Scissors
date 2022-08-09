67fb6614630e2542fba01bdc4e35e0eface53b4d
Felipe Domingos <Felipedomingosps@gmail.com>
2022-08-09 16:04:30 -0300

    Remove getWinner()

--- script.js
+++ script.js
@@ -179,39 +179,4 @@ if (winner === 'player') {
     }
 }
 
-// See who won the entire game
-async function getWinner(playerPoints,pcPoints) {
-if (playerPoints > pcPoints) {
-    let text = document.getElementById('round-text');
-    text.innerHTML = 'Player Wins!'
-
-    // > Add container animation later      
-
-    addClass('scaleUp','hand-img-pc');
-    addClass('scaleUp','hand-img-player');
-    await sleep(100)
-    changeImg('hand-img-player', 'smile')
-    changeImg('hand-img-pc', 'sad')
-    await sleep(500)
-    removeClass('scaleUp','hand-img-pc');
-    removeClass('scaleUp','hand-img-player');
-} else {
-    let text = document.getElementById('round-text');
-    text.innerHTML = 'PC Wins!'
-    
-    //Add Later: container animation
-
-    addClass('scaleUp','hand-img-pc');
-    addClass('scaleUp','hand-img-player');
-    await sleep(100)
-    changeImg('hand-img-pc', 'smile')
-    changeImg('hand-img-player', 'sad')
-    await sleep(500)
-    removeClass('scaleUp','hand-img-pc');
-    removeClass('scaleUp','hand-img-player');
-
-    //Add Later: Await
-    //Add Later: END of the game
-}
-}
 
